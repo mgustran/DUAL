83e1d34d306e3a084024f36901b74a87d6cf7ad2
miquel gustran <mgustran@gmail.com>
2016-03-09 18:30:21 +0100

    añdidos cambios

--- ProgramacionJava/Interfaces/src/clases/Pato.java
+++ ProgramacionJava/Interfaces/src/clases/Pato.java
@@ -1,5 +1,11 @@
 package clases;
 
-public class Pato {
-
+public class Pato implements Walkable {
+private String name;
+public Duck(String name) {
+this.name = name;
+}
+public void walk() {
+System.out.println(name + " (a duck) is walking.");
+}
 }
--- ProgramacionJava/Interfaces/src/clases/Persona.java
+++ ProgramacionJava/Interfaces/src/clases/Persona.java
@@ -1,5 +1,14 @@
 package clases;
+import interfaz.Walkable;
 
-public class Persona {
+public class Persona implements Walkable {
+	private String name;
 
+		public Persona(String name) {
+			this.name = name;
+		}
+		
+		public void walk() {
+			System.out.println(name + " (a person) is walking.");
+		}
 }
