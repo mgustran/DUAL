079fe8cc5b598d272464d04a7078cf09f683e8a5
miquel gustran <mgustran@gmail.com>
2016-03-17 17:16:38 +0100

    Inicio de ejercicio OXO en java

---
+++ ProgramacionJava/OXO/src/org/furious04/oxo/OXO.java
@@ -0,0 +1,17 @@
+package org.furious04.oxo;
+
+public class OXO {
+	int col = 0;
+	int row = 0;
+	public dadaPosicionMeterFicha(int col, int row){
+		if (col || row <= 3 )
+			
+		
+				
+				
+				
+				
+				return 
+	}
+
+}
---
+++ ProgramacionJava/OXO/src/org/furious04/oxo/OXOTest.java
@@ -0,0 +1,14 @@
+package org.furious04.oxo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OXOTest {
+
+	@Test
+	public void ponerFicha(4,1) {
+		;
+	}
+
+}
